c3470ba6b4ac10b145f932012ff3fd8f2e1fdff2
Fortunechinenyem <fortunechinenyem@gmail.com>
2024-12-21 14:45:19 +0100

    update

--- pages/auctions/index.js
+++ pages/auctions/index.js
@@ -1,32 +1,32 @@
-// import AuctionCard from "@/app/components/auction/AuctionCard";
-// import { useEffect, useState } from "react";
+import AuctionCard from "@/app/components/auction/AuctionCard";
+import { useEffect, useState } from "react";
 
-// const AuctionPage = () => {
-//   const [cars, setCars] = useState([]);
+const AuctionPage = () => {
+  const [cars, setCars] = useState([]);
 
-//   useEffect(() => {
-//     fetch("/api/auctions")
-//       .then((res) => res.json())
-//       .then((data) => {
-//         if (Array.isArray(data)) {
-//           setCars(data);
-//         } else {
-//           console.error("Expected an array, got:", data);
-//         }
-//       })
-//       .catch((error) => console.error("Failed to fetch auctions:", error));
-//   }, []);
+  useEffect(() => {
+    fetch("/api/auctions")
+      .then((res) => res.json())
+      .then((data) => {
+        if (Array.isArray(data)) {
+          setCars(data);
+        } else {
+          console.error("Expected an array, got:", data);
+        }
+      })
+      .catch((error) => console.error("Failed to fetch auctions:", error));
+  }, []);
 
-//   return (
-//     <div className="container mx-auto py-8">
-//       <h1 className="text-4xl font-semibold text-center mb-8">
-//         Ongoing Auctions
-//       </h1>
-//       {cars.map((car) => (
-//         <AuctionCard key={car._id} car={car} />
-//       ))}
-//     </div>
-//   );
-// };
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-4xl font-semibold text-center mb-8">
+        Ongoing Auctions
+      </h1>
+      {cars.map((car) => (
+        <AuctionCard key={car._id} car={car} />
+      ))}
+    </div>
+  );
+};
 
-// export default AuctionPage;
+export default AuctionPage;
