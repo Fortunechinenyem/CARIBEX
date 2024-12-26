6fa108bb9fbdcf56a7b43ef10832a1136d2a8658
Fortunechinenyem <fortunechinenyem@gmail.com>
2024-12-26 17:46:24 +0100

    fix:build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+// module.exports = {
+//   env: {
+//     NEXT_PUBLIC_STRIPE_PUBLIC_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY,
+//   },
+// };
 
 export default nextConfig;
